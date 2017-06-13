159ab476f8ecf48e7bbe133f7a7051d719191939
Raphael Jeongho Eom <gblue1223@gmail.com>
2017-06-13 22:43:21 +0900

    # working on login panel

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import oktopusLogo from './img/oktopus-logo.svg';
-import _1Pagecenter from './img/1-pagecenter.svg';
+import oktopusLoginLogo1x from './img/screen-shot-2017-02-11-at-7-08-42-pm.png';
+import oktopusLoginLogo2x from './img/screen-shot-2017-02-11-at-7-08-42-pm@2x.png';
+import oktopusLoginLogo3x from './img/screen-shot-2017-02-11-at-7-08-42-pm@3x.png';
+import pagecenter1 from './img/1-pagecenter@3x.png';
 import sass from './styles.scss';
 
 export default class Login extends React.Component {
   render() {
     return (
       <div className={sass.rectangle}>
-        <div className={sass.topbarBg}>
-          <img src={oktopusLogo} alt="Oktopus" className={sass.oktopusLogo} />
-          <img src={_1Pagecenter} alt="Signup or login" className={sass._1Pagecenter} />
+        <div className={sass['topbar-bg']}>
+          <img src={oktopusLogo} alt="Oktopus" className={sass['oktopus-logo']} />
+          <img src={pagecenter1} alt="Signup or login" className={sass['pagecenter-1']} />
+        </div>
+
+        <div className={sass['your-website-is-about']}>
+          Your website is about to be reborn.
+        </div>
+
+        <div className={sass['combined-shape']}>
+          <img
+            src={oktopusLoginLogo1x}
+            srcSet={`${oktopusLoginLogo1x} 1x, ${oktopusLoginLogo1x} 2x, ${oktopusLoginLogo3x} 3x`}
+            className={sass['screen-shot-2017-02-11-at-7-08-42-pm']}
+            alt="Oktopus"
+          />
         </div>
       </div>
     );
